fff055a2d7b45b8cb519aae9e474e1883a0a43d3
yexiaoyou <860262295@qq.com>
2016-05-13 20:02:44 +0800

    add auto dissmiss controler

--- player/src/main/java/com/ye/player/player/ui/PlayerActivity.java
+++ player/src/main/java/com/ye/player/player/ui/PlayerActivity.java
@@ -1,30 +1,21 @@
 package com.ye.player.player.ui;
 
-import android.content.pm.ActivityInfo;
-import android.graphics.Canvas;
+import android.content.Context;
 import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.os.Environment;
-import android.text.Spannable;
+import android.os.Handler;
 import android.text.SpannableStringBuilder;
-import android.text.Spanned;
-import android.text.TextPaint;
-import android.text.style.BackgroundColorSpan;
-import android.text.style.ImageSpan;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.PopupWindow;
 import android.widget.TextView;
-import android.widget.VideoView;
 
 import com.ye.player.R;
 import com.ye.player.common.bean.VideoInfo;
@@ -36,11 +27,7 @@ import com.ye.player.player.widget.MyVideoView;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.HashMap;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -53,15 +40,12 @@ import master.flame.danmaku.danmaku.model.BaseDanmaku;
 import master.flame.danmaku.danmaku.model.DanmakuTimer;
 import master.flame.danmaku.danmaku.model.IDanmakus;
 import master.flame.danmaku.danmaku.model.IDisplayer;
-import master.flame.danmaku.danmaku.model.android.BaseCacheStuffer;
 import master.flame.danmaku.danmaku.model.android.DanmakuContext;
 import master.flame.danmaku.danmaku.model.android.Danmakus;
 import master.flame.danmaku.danmaku.model.android.SpannedCacheStuffer;
 import master.flame.danmaku.danmaku.parser.BaseDanmakuParser;
 import master.flame.danmaku.danmaku.parser.IDataSource;
 import master.flame.danmaku.danmaku.parser.android.BiliDanmukuParser;
-import master.flame.danmaku.danmaku.util.IOUtils;
-import master.flame.danmaku.danmaku.util.SystemClock;
 
 public class PlayerActivity extends BaseActivity implements View.OnClickListener {
 
@@ -87,107 +71,17 @@ public class PlayerActivity extends BaseActivity implements View.OnClickListener
 
     private DanmakuContext mContext;
 
-
-    public PopupWindow mPopupWindow;
-
-    private Button mBtnRotate;
-
-    private Button mBtnHideDanmaku;
-
-    private Button mBtnShowDanmaku;
-
     private BaseDanmakuParser mParser;
-
-    private Button mBtnPauseDanmaku;
-
-    private Button mBtnResumeDanmaku;
-
-    private Button mBtnSendDanmaku;
-
-    private Button mBtnSendDanmakuTextAndImage;
-
-    private ImageButton mBtnBack;
-
-    private TextView mTextViewTitle;
-
-    private Button mBtnSendDanmakus;
 
     private CacheStufferAdapter mCacheStufferAdapter = new CacheStufferAdapter(mDanmakuView);
 
+    private static final int DELAY_MISS = 3000;
 
-   /* private BaseCacheStuffer.Proxy mCacheStufferAdapter = new BaseCacheStuffer.Proxy() {
+    private Handler mHandler = new Handler();
 
-        private Drawable mDrawable;
+    private Timer timer= new Timer();
 
-        @Override
-        public void prepareDrawing(final BaseDanmaku danmaku, boolean fromWorkerThread) {
-            if (danmaku.text instanceof Spanned) { // 根据你的条件检查是否需要需要更新弹幕
-                // FIXME 这里只是简单启个线程来加载远程url图片，请使用你自己的异步线程池，最好加上你的缓存池
-                new Thread() {
-
-                    @Override
-                    public void run() {
-                        String url = "http://www.bilibili.com/favicon.ico";
-                        InputStream inputStream = null;
-                        Drawable drawable = mDrawable;
-                        if(drawable == null) {
-                            try {
-                                URLConnection urlConnection = new URL(url).openConnection();
-                                inputStream = urlConnection.getInputStream();
-                                drawable = BitmapDrawable.createFromStream(inputStream, "bitmap");
-                                mDrawable = drawable;
-                            } catch (MalformedURLException e) {
-                                e.printStackTrace();
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            } finally {
-                                IOUtils.closeQuietly(inputStream);
-                            }
-                        }
-                        if (drawable != null) {
-                            drawable.setBounds(0, 0, 100, 100);
-                            SpannableStringBuilder spannable = createSpannable(drawable);
-                            danmaku.text = spannable;
-                            if(mDanmakuView != null) {
-                                mDanmakuView.invalidateDanmaku(danmaku, false);
-                            }
-                            return;
-                        }
-                    }
-                }.start();
-            }
-        }
-
-        @Override
-        public void releaseResource(BaseDanmaku danmaku) {
-            // TODO 重要:清理含有ImageSpan的text中的一些占用内存的资源 例如drawable
-        }
-    };*/
-
-    /**
-     * 绘制背景(自定义弹幕样式)
-     */
-    private static class BackgroundCacheStuffer extends SpannedCacheStuffer {
-        // 通过扩展SimpleTextCacheStuffer或SpannedCacheStuffer个性化你的弹幕样式
-        final Paint paint = new Paint();
-
-        @Override
-        public void measure(BaseDanmaku danmaku, TextPaint paint, boolean fromWorkerThread) {
-            danmaku.padding = 10;  // 在背景绘制模式下增加padding
-            super.measure(danmaku, paint, fromWorkerThread);
-        }
-
-        @Override
-        public void drawBackground(BaseDanmaku danmaku, Canvas canvas, float left, float top) {
-            paint.setColor(0x8125309b);
-            canvas.drawRect(left + 2, top + 2, left + danmaku.paintWidth - 2, top + danmaku.paintHeight - 2, paint);
-        }
-
-        @Override
-        public void drawStroke(BaseDanmaku danmaku, String lineText, Canvas canvas, float left, float top, Paint paint) {
-            // 禁用描边绘制
-        }
-    }
+    private TimeOutTask task;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -195,7 +89,9 @@ public class PlayerActivity extends BaseActivity implements View.OnClickListener
         setContentView(R.layout.activity_player);
         videoInfo = (VideoInfo) getIntent().getSerializableExtra("videoInfo");
         initViews();
-        findViews();
+        setDanmaku();
+        task = new TimeOutTask();
+        timer.schedule(task,DELAY_MISS);
     }
 
     private void initViews() {
@@ -246,7 +142,7 @@ public class PlayerActivity extends BaseActivity implements View.OnClickListener
 
     }
 
-    private void findViews() {
+    private void setDanmaku() {
         // 设置最大显示行数
         HashMap<Integer, Integer> maxLinesPair = new HashMap<Integer, Integer>();
         maxLinesPair.put(BaseDanmaku.TYPE_SCROLL_RL, 5); // 滚动弹幕最大显示3行
@@ -314,6 +210,7 @@ public class PlayerActivity extends BaseActivity implements View.OnClickListener
                 @Override
                 public void onClick(View view) {
                     mMediaController.setVisibility(View.VISIBLE);
+                    delayDissController();
                 }
             });
         }
@@ -336,6 +233,9 @@ public class PlayerActivity extends BaseActivity implements View.OnClickListener
         if (mDanmakuView != null && mDanmakuView.isPrepared()) {
             mDanmakuView.pause();
         }
+        if (mVideoView != null){
+            mVideoView.pause();
+        }
     }
 
     @Override
@@ -343,6 +243,9 @@ public class PlayerActivity extends BaseActivity implements View.OnClickListener
         super.onResume();
         if (mDanmakuView != null && mDanmakuView.isPrepared() && mDanmakuView.isPaused()) {
             mDanmakuView.resume();
+        }
+        if (mVideoView != null){
+            mVideoView.start();
         }
     }
 
@@ -377,9 +280,8 @@ public class PlayerActivity extends BaseActivity implements View.OnClickListener
 
         if (v == mMediaController) {
             mMediaController.setVisibility(View.GONE);
+            hideKeyBoard();
         }
-       /* if (mDanmakuView == null || !mDanmakuView.isPrepared())
-            return;*/
         if (v == btnBack) {
             this.finish();
         } else if (v ==btnPause) {
@@ -394,6 +296,7 @@ public class PlayerActivity extends BaseActivity implements View.OnClickListener
             }
         } else if (v == btnSend && mDanmakuView != null && mDanmakuView.isPrepared()){
             sendComment();
+            delayDissController();
         } else if (v ==btnHide && mDanmakuView != null && mDanmakuView.isPrepared()){
             if (btnHide.isChecked()) {
                 mDanmakuView.hide();
@@ -401,85 +304,71 @@ public class PlayerActivity extends BaseActivity implements View.OnClickListener
                 mDanmakuView.show();
             }
         }
-
-
-
-
-
-
-
-
-
-
-
-
-        if (v == mBtnRotate) {
-            setRequestedOrientation(getRequestedOrientation() == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE ? ActivityInfo.SCREEN_ORIENTATION_PORTRAIT : ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        } else if (v == mBtnHideDanmaku) {
-            mDanmakuView.hide();
-            // mPausedPosition = mDanmakuView.hideAndPauseDrawTask();
-        } else if (v == mBtnShowDanmaku) {
-            mDanmakuView.show();
-            // mDanmakuView.showAndResumeDrawTask(mPausedPosition); // sync to the video time in your practice
-        } else if (v == mBtnPauseDanmaku) {
-            mDanmakuView.pause();
-        } else if (v == mBtnResumeDanmaku) {
-            mDanmakuView.resume();
-        } else if (v == mBtnSendDanmaku) {
-            addDanmaku(false);
-        } else if (v == mBtnSendDanmakuTextAndImage) {
-            addDanmaKuShowTextAndImage(false);
-        } else if (v == mBtnSendDanmakus) {
-            Boolean b = (Boolean) mBtnSendDanmakus.getTag();
-            timer.cancel();
-            if (b == null || !b) {
-                mBtnSendDanmakus.setText(R.string.cancel_sending_danmakus);
-                timer = new Timer();
-                timer.schedule(new AsyncAddTask(), 0, 1000);
-                mBtnSendDanmakus.setTag(true);
-            } else {
-                mBtnSendDanmakus.setText(R.string.send_danmakus);
-                mBtnSendDanmakus.setTag(false);
-            }
-        }
     }
 
     private void sendComment() {
+        String comment = editTextComment.getText().toString();
+        if (null == comment||StringUtil.isEmpty(comment)) {
+            return;
+        }
+        sendCommentToServer(comment);
+        addDanmaku(comment);
+        editTextComment.setText("");
+        editTextComment.clearFocus();
+        hideKeyBoard();
 
     }
 
-    Timer timer = new Timer();
+    private void hideKeyBoard() {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(editTextComment.getWindowToken(), 0);
+    }
 
-    class AsyncAddTask extends TimerTask {
+    private void sendCommentToServer(String comment) {
 
-        @Override
-        public void run() {
-            for (int i = 0; i < 20; i++) {
-                addDanmaku(true);
-                SystemClock.sleep(20);
-            }
-        }
-    };
+    }
 
-    private void addDanmaku(boolean islive) {
+
+    private void addDanmaku(String comment){
         BaseDanmaku danmaku = mContext.mDanmakuFactory.createDanmaku(BaseDanmaku.TYPE_SCROLL_RL);
         if (danmaku == null || mDanmakuView == null) {
             return;
         }
-        // for(int i=0;i<100;i++){
-        // }
-        danmaku.text = "这是一条弹幕" + System.nanoTime();
+        //danmaku.text = "这是一条弹幕" + System.nanoTime();
+        danmaku.text =comment;
         danmaku.padding = 5;
-        danmaku.priority = 0;  // 可能会被各种过滤器过滤并隐藏显示
-        danmaku.isLive = islive;
-        danmaku.time = mDanmakuView.getCurrentTime() + 1200;
-        danmaku.textSize = 25f * (mParser.getDisplayer().getDensity() - 0.6f);
+        danmaku.priority = 10;  // 不会被各种过滤器过滤并隐藏显示
+        danmaku.isLive = false;
+        danmaku.time = mDanmakuView.getCurrentTime() + 600;
+        danmaku.textSize = 30f * (mParser.getDisplayer().getDensity() - 0.6f);
         danmaku.textColor = Color.RED;
-        danmaku.textShadowColor = Color.WHITE;
+        // danmaku.textShadowColor = Color.WHITE;
         // danmaku.underlineColor = Color.GREEN;
-        danmaku.borderColor = Color.GREEN;
+        // danmaku.borderColor = Color.GREEN;
         mDanmakuView.addDanmaku(danmaku);
+    }
 
+    private void delayDissController() {
+        task.cancel();
+        task = new TimeOutTask();
+        timer.schedule(task,DELAY_MISS);
+    }
+
+    public class TimeOutTask extends TimerTask{
+
+        @Override
+        public void run() {
+            mHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    if (!editTextComment.hasFocus()&&mMediaController!=null){
+                        mMediaController.setVisibility(View.GONE);
+                    } else {
+                        timer.cancel();
+                    }
+                }
+            });
+        }
     }
 
     private void addDanmaKuShowTextAndImage(boolean islive) {
@@ -499,15 +388,6 @@ public class PlayerActivity extends BaseActivity implements View.OnClickListener
         mDanmakuView.addDanmaku(danmaku);
     }
 
-    /*private SpannableStringBuilder createSpannable(Drawable drawable) {
-        String text = "bitmap";
-        SpannableStringBuilder spannableStringBuilder = new SpannableStringBuilder(text);
-        ImageSpan span = new ImageSpan(drawable);//ImageSpan.ALIGN_BOTTOM);
-        spannableStringBuilder.setSpan(span, 0, text.length(), Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
-        spannableStringBuilder.append("图文混排");
-        spannableStringBuilder.setSpan(new BackgroundColorSpan(Color.parseColor("#8A2233B1")), 0, spannableStringBuilder.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
-        return spannableStringBuilder;
-    }*/
 
     public boolean hasNavigationBar() {
         return false;
